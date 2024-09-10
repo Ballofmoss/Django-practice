0f4d6413e17c08f7738786a8ae1f2dff5cefe097
студент211 <211@mveu.local>
2024-09-10 11:27:21 +0400

    practic4

---
+++ library/models/__init__.py
@@ -0,0 +1,15 @@
+from .author import Author
+from .book import Book
+from .publisher import Publisher
+from .student import Student
+from .teacher import Teacher
+from .course import Course
+
+__all__ =(
+    'Author',
+    'Book',
+    'Publisher',
+    'Student',
+    'Teacher',
+    'Course',
+)
