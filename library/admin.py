686646ff89f55c37278fe4b84d173c4dd99836d9
студент308 <308@mveu.local>
2024-09-11 09:38:32 +0400

    pr5

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from library.models import Author, Book, Publisher
 
 # Register your models here.
+
+@admin.register(Author)
+class AdminAuthor(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class AdminBook(admin.ModelAdmin):
+    pass
+
+@admin.register(Publisher)
+class AdminPublisher(admin.ModelAdmin):
+    pass
+    
