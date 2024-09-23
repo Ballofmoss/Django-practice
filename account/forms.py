859c53738a8cd5938695ddaef4de109ae98f3898
Ballofmoss <ivansvenn@ya.ru>
2024-09-23 11:59:19 +0400

    practice12

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
@@ -62,3 +62,43 @@ class RegisterForm(UserCreationForm):
         
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        max_length=254,
+        widget = forms.TextInput(
+        attrs={
+            'autocomplete': 'text',
+            'placeholder': 'Логин',
+            }
+        ),
+        required=False
+    )
+    password = forms.CharField(
+        widget = forms.PasswordInput(
+            attrs={
+                "autocomplete": "current-password",
+                'placeholder': 'Пароль'
+            }
+        ),
+        required=False
+    )
+    
+    error_messages = {
+        "invalid_login": (
+            "Введите логин и пароль правильно"
+        ),
+    }
+    
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        if password == '':
+            raise forms.ValidationError('Введите пароль', code='invalid')
+        return password
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if username == '':
+            raise forms.ValidationError('Введите логин', code='invalid')
+        if not User.objects.filter(username=username):
+            raise forms.ValidationError('Такого пользователя не существует', code='invalid')
+        return username
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm, LoginForm
 from django.http import HttpRequest
 from django.contrib.auth.models import User
 
+# jaba
+# -BeM8GueMJ
 def register(request:HttpRequest):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -20,5 +22,23 @@ def register(request:HttpRequest):
             'title': 'Регистрация',
             'form': form,
         })
+        
+def login_user(request:HttpRequest):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else: 
+        form = LoginForm()
+    return render(request, 'login.html', context={
+        'title': 'Авторизация',
+        'form': form,
+    })
+def logout_user(request:HttpRequest):
+        logout(request)
+        return redirect('home')
 
 
