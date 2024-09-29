64668534c158704a934f95bab3ad836801384bdf
Ballofmoss <ivansvenn@ya.ru>
2024-09-29 17:04:03 +0400

    pr16

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .forms import RegisterForm
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestCaseViewAccount(TestCase):
+    
+    def setUp(self) -> None:
+        self.form_data ={
+            'username' : 'DGFDWEFWEF',
+            'email' : 'frwefwefwef@gmail.com',
+            'password1' : 'Qweqwesdfsdf123444',
+            'password2' : 'Qweqwesdfsdf123444'
+        }
+    def test_register_view(self):
+        
+        RegisterForm(data=self.form_data)
+        self.client.post(reverse('register'), data=self.form_data)
+        user = User.objects.filter(username=self.form_data['username']).exists()
+        self.assertTrue(user)
