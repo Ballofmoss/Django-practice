fc2b54f87e6724895632ef271ee43cd9a0beab7c
студент211 <211@mveu.local>
2024-10-08 11:20:21 +0400

    oop2

---
+++ MethodSelf.py
@@ -0,0 +1,35 @@
+import re
+
+
+class UserSchem: pass
+
+class DataBase: 
+    users_data = []
+    def get_data(self):
+        with open('pivo.txt', 'r', encoding='UTF=8') as f:
+            result =f.readlines()
+            f.close()
+            return result
+    def serializers(self, data):
+        content = []
+        for i in data:
+            schema=dict()
+            line = [i for i in re.split(r'\s',i) if i != '']
+            for index in range(0,len(line)-1, 2):
+                schema[line[index]] = line[index+1]
+            content.append(schema)
+            return content
+    def create(self, data):
+        for i in data:
+            user = UserSchem()
+            for key, item in i.items():
+                setattr(user, key, item)
+                self.users_data.append(user)
+
+daata = DataBase()
+text = daata.get_data()
+daata.create(text)
+print(daata.users_data)
+ 
+        
+            
