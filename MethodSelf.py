ff4dd52b88134ea397c827b4722b6ae45791bd17
студент211 <211@mveu.local>
2024-10-09 11:14:56 +0400

    oop2.3

--- MethodSelf.py
+++ MethodSelf.py
@@ -4,7 +4,7 @@ import re
 class UserSchem: pass
 
 class DataBase: 
-    users_data = []
+    users_data:list = []
     def get_data(self, url):
         with open(url, 'r', encoding='UTF=8') as f:
             result = f.readlines()
@@ -14,23 +14,41 @@ class DataBase:
         content = []
         for i in data:
             schema=dict()
-            line = [i for i in re.split(r'\s',i) if i != '']
+            line = [i for i in re.split(r'\s',i) if i != ' ']
             for index in range(0,len(line)-1, 2):
                 schema[line[index]] = line[index+1]
             content.append(schema)
-            return content
+        print(content)
+        return content
     def create(self, data):
         for i in data:
             user = UserSchem()
             for key, item in i.items():
                 setattr(user, key, item)
-                self.users_data.append(user)
-    # def search()
-
-daata = DataBase()
-text = daata.get_data('pivo.txt')
-daata.create(text)
-print(daata.users_data)
+            self.users_data.append(user)
+    def search(self, search):
+        for user in self.users_data:
+            for _, value in user.__dier__.items():
+                if search in str(value):
+                    return user
+        return None
+class Translator: 
+    dictionary: dict = {}
+    def add(self, eng: str, rus: str):
+        if self.dictionary.get
+        dict.append
+        
+        
+    
+            
+            
+            
+            
+# daata = DataBase()
+# text = daata.get_data('pivo.txt')
+# text = daata.serializers(text)
+# print(daata.create(text))
+# print(daata.users_data[2].__dict__)
 
 
  
--- pr7/pr7/baza/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_base
-from .views import o_nas
-
-urlpatterns = [
-path('', get_base),
-path('onas/', o_nas, name='onas'),
-]
